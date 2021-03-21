c97119d894b5170aede14497f5e3594e424db39f
Imran <imranbashamg@gmail.com>
2021-03-21 21:52:38 +0530

    Initial commit of Product Application

---
+++ ProductApplication/src/main/java/com/imrantechsoft/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.imrantechsoft;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ ProductApplication/src/main/java/com/imrantechsoft/ProductService.java
@@ -0,0 +1,33 @@
+package com.imrantechsoft;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repo;
+	
+	public List<Product> listAll() {
+		return repo.findAll();
+	}
+	
+	public void save(Product product) {
+		if(product != null && product.getName() != null && product.getBrand() != null && product.getMadein() != null && product.getPrice() != "") {
+		repo.save(product);
+		}
+	}
+	
+	public Product get(long id) {
+		return repo.findById(id).get();
+	}
+	
+	public void delete(long id) {
+		repo.deleteById(id);
+	}
+}
